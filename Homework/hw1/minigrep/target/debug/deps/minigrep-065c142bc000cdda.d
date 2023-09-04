03b0da7c66958e177c9ebad13df86a20a91cbf9d
YaQ1an9 <1185365215@qq.com>
2023-09-04 17:17:06 +0800

    Homework_1 Begin

---
+++ Homework/hw1/minigrep/target/debug/deps/minigrep-065c142bc000cdda.d
@@ -0,0 +1,5 @@
+/Users/dn_csj/RUST/Rust-Summer/Homework/hw1/minigrep/target/debug/deps/minigrep-065c142bc000cdda.rmeta: src/main.rs
+
+/Users/dn_csj/RUST/Rust-Summer/Homework/hw1/minigrep/target/debug/deps/minigrep-065c142bc000cdda.d: src/main.rs
+
+src/main.rs:
